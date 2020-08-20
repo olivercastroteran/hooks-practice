3ea86a5f0ffaaa3664709c84d13365e6c89b2e77
olivercastroteran <olivercastroteran@gmail.com>
2020-08-20 16:00:17 -0400

    filter by title functionality implemented

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,21 +7,21 @@ import Search from './Search';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://hooks-644ce.firebaseio.com/ingredients.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const loadedIngredients = [];
-        for (const key in data) {
-          loadedIngredients.push({
-            id: key,
-            title: data[key].title,
-            amount: data[key].amount,
-          });
-        }
-        setIngredients(loadedIngredients);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://hooks-644ce.firebaseio.com/ingredients.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const loadedIngredients = [];
+  //       for (const key in data) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: data[key].title,
+  //           amount: data[key].amount,
+  //         });
+  //       }
+  //       setIngredients(loadedIngredients);
+  //     });
+  // }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch('https://hooks-644ce.firebaseio.com/ingredients.json', {
@@ -46,12 +46,16 @@ const Ingredients = () => {
     );
   };
 
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setIngredients(filteredIngredients);
+  }, []);
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={ingredients}
           onRemoveItem={removeIngredientHandler}
